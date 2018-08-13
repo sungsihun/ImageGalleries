b104ac887df2ee759a81c116a71ea36c3b3607db
SiHun Sung <sungsihun@gmail.com>
2018-08-13 16:33:51 -0400

    part 1

--- ImageGalleries/ViewController.m
+++ ImageGalleries/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, weak) UIImageView *imageView1;
+@property (nonatomic, weak) UIImageView *imageView2;
+@property (nonatomic, weak) UIImageView *imageView3;
 
 @end
 
@@ -16,14 +20,75 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // image 1
+    UIImage *image1 = [UIImage imageNamed:@"Lighthouse-in-Field.jpg"];
+    UIImageView *imageView1 = [[UIImageView alloc] initWithImage:image1];
+    imageView1.contentMode = UIViewContentModeScaleAspectFit;
+    imageView1.clipsToBounds = YES;
+    self.imageView1 = imageView1;
+    [self.scrollView addSubview:self.imageView1];
+    
+    self.imageView1.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.imageView1.topAnchor constraintEqualToAnchor:self.scrollView.topAnchor].active = YES;
+    [self.imageView1.bottomAnchor constraintEqualToAnchor:self.scrollView.bottomAnchor].active = YES;
+    [self.imageView1.leadingAnchor constraintEqualToAnchor:self.scrollView.leadingAnchor].active = YES;
+//    [self.imageView1.trailingAnchor constraintEqualToAnchor:self.imageView2.leadingAnchor].active = YES;
+    
+    
+    [self.imageView1.widthAnchor constraintEqualToAnchor:self.scrollView.widthAnchor].active = YES;
+    [self.imageView1.heightAnchor constraintEqualToAnchor:self.scrollView.heightAnchor].active = YES;
+    
+    
+    
+
+
+    // image 2
+    UIImage *image2 = [UIImage imageNamed:@"Lighthouse-night.jpg"];
+    UIImageView *imageView2 = [[UIImageView alloc] initWithImage:image2];
+    imageView2.contentMode = UIViewContentModeScaleAspectFill;
+    imageView2.clipsToBounds = YES;
+    self.imageView2 = imageView2;
+    [self.scrollView addSubview:self.imageView2];
+
+    self.imageView2.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.imageView2.topAnchor constraintEqualToAnchor:self.scrollView.topAnchor].active = YES;
+    [self.imageView2.bottomAnchor constraintEqualToAnchor:self.scrollView.bottomAnchor].active = YES;
+    [self.imageView2.leadingAnchor constraintEqualToAnchor:self.imageView1.trailingAnchor].active = YES;
+//    [self.imageView2.trailingAnchor constraintEqualToAnchor:self.imageView3.leadingAnchor].active = YES;
+    
+    [self.imageView2.widthAnchor constraintEqualToAnchor:self.scrollView.widthAnchor].active = YES;
+    [self.imageView2.heightAnchor constraintEqualToAnchor:self.scrollView.heightAnchor].active = YES;
+    
+
+
+
+    // image 3
+    UIImage *image3 = [UIImage imageNamed:@"Lighthouse-zoomed"];
+    UIImageView *imageView3 = [[UIImageView alloc] initWithImage:image3];
+    imageView3.contentMode = UIViewContentModeScaleAspectFill;
+    imageView3.clipsToBounds = YES;
+    self.imageView3 = imageView3;
+    [self.scrollView addSubview:self.imageView3];
+
+    self.imageView3.translatesAutoresizingMaskIntoConstraints = NO;
+
+    [self.imageView3.topAnchor constraintEqualToAnchor:self.scrollView.topAnchor].active = YES;
+    [self.imageView3.bottomAnchor constraintEqualToAnchor:self.scrollView.bottomAnchor].active = YES;
+    [self.imageView3.leadingAnchor constraintEqualToAnchor:self.imageView2.trailingAnchor].active = YES;
+    [self.imageView3.trailingAnchor constraintEqualToAnchor:self.scrollView.trailingAnchor].active = YES;
+    
+    [self.imageView3.widthAnchor constraintEqualToAnchor:self.scrollView.widthAnchor].active = YES;
+    [self.imageView3.heightAnchor constraintEqualToAnchor:self.scrollView.heightAnchor].active = YES;
+    
+
+    
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
